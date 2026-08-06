61dd0f42b8c9161dd52d337f666a1812ce113a84
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-06 16:17:06 +0530

    added in git

---
+++ chp1.py
@@ -0,0 +1,11 @@
+
+'''
+Python learning by following the Apna College yt video
+Link: https://youtu.be/q3AuP01daL4?si=xhJEHcg5ZFNxnHND
+
+Variables and how to print something in console
+
+'''
+
+print("Hello world")
+
