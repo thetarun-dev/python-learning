c54b7de8ff6671f075d761c1102f1a4f70cad90a
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-06 16:56:02 +0530

    modify
    learned about the variable and type of data

--- chp1.py
+++ chp1.py
@@ -6,7 +6,46 @@ It's used in backend, machine learning, AI and web development
 
 '''
 
-
 # for printing anything in console we use print()
-print("Hey Guys, I learned to print in python")
+# print("Hey Guys, I learned to print in python")
+
+# -------------------------------------------------------------------------------------
+
+'''
+
+Variables
+
+Variables are the containers which stored some value such as
+name = Tarun, here name is the container which store the Tarun as the value inside so whenever we will use
+name variables we will see Tarun in console as it's value is Tarun
+
+Unlike other programming language, in python we don't need to declare the type of the variable
+
+'''
+
+name = "Tarun"     # storing the string type of data
+age = 23           # storing the integer/int type of data
+height = 5.8       # storing the floating point/double type of data
+am_i_a_male = True # storing boolean type of data
+
+# print(name)
+print("Hello, My name is", name)
+print("And my age is", age)
+print("And my height is", height, "cm")
+print("Is Male:", am_i_a_male)
+
+'''
+
+Types of data
+string,
+Integer,
+Float - decimal no such as 99.0,
+Boolean - True or false
+
+'''
+# printing the type of data which each variable is holding
+print(type(name))        # will print str as this name variable hold the string type of data i.e tarun
+print(type(age))         # will print int
+print(type(height))      # will print float
+print(type(am_i_a_male)) # will print bool
 
