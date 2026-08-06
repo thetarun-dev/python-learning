450b5c8fe8222bdca34c367eecc8a96c83cc8155
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-06 17:10:00 +0530

    completed 1st exercise

---
+++ exercise/Exercise1.py
@@ -0,0 +1,16 @@
+
+# Exercise
+
+# Print a person whose name is tony, age is 53 and height is 1.85m
+# Tony is a superhero take his superhero name as input and print all of his details
+
+name = "Tony"
+age = 53
+height = 1.85
+
+superhero_name = input("Enter your superhero name: ")
+print("Hey,", name)
+print("Your age is:", age)
+print("Your height is:", height)
+print("Your superhero name is:", superhero_name)
+
