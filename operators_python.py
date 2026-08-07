d7796bdc70eda5962cf80a1b20131a30e0211cce
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-07 17:47:01 +0530

    learned arithmetic operator

---
+++ operators_python.py
@@ -0,0 +1,39 @@
+
+# Python operators
+
+'''
+
+Arithmetics operators
+
+Comparison Operators
+
+Logical Operators
+
+Conditional Operators
+
+'''
+
+
+# Arithmetics
+
+# add
+print(15 + 20)
+
+# Sub
+print(15 - 20)
+
+# Multuply
+print(15 * 20)
+
+# Divison
+print(15 / 20)
+
+# only give integer value in Divison
+print(15 // 20)
+
+# modulo => Reminder
+print(15 % 20)
+
+# power
+print(5 ** 3)
+
