b13e7dd765d538060e60a64c2d6f9e954602404a
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-07 18:15:14 +0530

    learned if statements

--- conditional_stat.py
+++ conditional_stat.py
@@ -1,6 +1,24 @@
 
 # Conditional Statements
 
+'''
 
+Conditional Statements are the statements in which some conditional are check to before performing a operations.
+
+'''
+
+# if statements
+
+# Let check whether the age is 18 or nots
+my_age = 22
+if my_age >= 18:
+    print("Yes, I am old enough to take my decision like a man")
+
+'''
+
+Here, first the conditional will be checked that whether the my_age is greater or equal to 18 and if yes then only it will
+print else it will be ignoreds
+
+'''
 
 
