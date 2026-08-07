326ad4a75d81607d85b5d87d9d76cf4ffed05661
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-07 17:35:45 +0530

    added exercise2

---
+++ exercise/Exercise2.py
@@ -0,0 +1,20 @@
+'''
+
+Exercise 2
+
+1. Take the price of 3 product as input and print total price and average price
+
+2. Take superhero name and check wehther it is start with S or s 
+
+'''
+
+num1 = int(input("Enter amount: "))
+num2 = int(input("Enter amount: "))
+num3 = int(input("Enter amount: "))
+
+Sum = num1 + num2 + num3
+average = Sum / 3
+
+print("Total bill amount:", Sum)
+print("Average price:", average)
+
