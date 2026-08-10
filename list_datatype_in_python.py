0ac9c5581bf4ae6bddf4a517102c8fdb6dd6f559
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 03:24:48 +0530

    modify

--- list_datatype_in_python.py
+++ list_datatype_in_python.py
@@ -19,15 +19,19 @@ Link: https://www.w3schools.com/python/python_lists.asp
 list1 = [1, "Tarun", 89.89, False, 3]
 print(list1)
 
+# -------------------------------------------------------------------------------------
 # length() => It will provide the length of the list/no of items
 print("Length of list1 is:", len(list1))      # 5
 
+# -------------------------------------------------------------------------------------
 # accessing the item of list using the index no
 print("Value at 3 index is:", list1[3])      # False
 
+# -------------------------------------------------------------------------------------
 # accessing the item of list using negative index
 print("Value stored at -3 is:", list1[-3])   # 89.89
 
+# -------------------------------------------------------------------------------------
 # accessing the item of list by range
 print("Value stored from 0 to 4 are: ", list1[:4])
 print("Value stored from 2 to end are:", list1[2:])
@@ -37,6 +41,7 @@ print("Current value stored in list:", list1)
 list1[4] = 90
 print("After changing the value:", list1)
 
+# -------------------------------------------------------------------------------------
 # adding the items in list using append(), insert() and extend()
 
 # append() => add item at end of list
@@ -55,4 +60,7 @@ list2 = ["list2", "Bhandari", 73]
 print("List 2: ", list2)
 list1.extend(list2)
 print("Extending the list1 using the list2:", list1)
+
+# -------------------------------------------------------------------------------------
+
 
