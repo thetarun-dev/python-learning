fbf24e93f91c165d08f347043cfdcb0028f9b239
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 03:01:55 +0530

    learned about the list datatype

---
+++ list_datatype_in_python.py
@@ -0,0 +1,15 @@
+
+# List Datatype
+
+'''
+
+List is a collection/sequence of same or different type of the data store in [] and separated by ','
+Example: [1, "Tarun", 89.89, False, 3]
+
+Like the array the list index start with 0
+
+'''
+
+list1 = [1, "Tarun", 89.89, False, 3]
+print(list1)
+
