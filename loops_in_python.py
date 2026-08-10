a6bb3975591ccea30aa91e4a3822622589a3f4f7
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 02:11:35 +0530

    learned while loop

--- loops_in_python.py
+++ loops_in_python.py
@@ -7,3 +7,23 @@ print(num)
 
 # -------------------------------------------------------------------------------------
 # while loop
+
+'''
+
+Syntax
+
+while condition:
+    do something...
+
+Example
+'''
+counter = 0
+
+while counter <= 5: # condition is counter should be smaller or equal to 5
+    print(counter)  # print counter value
+    counter += 1    # then increase counter value
+
+# which will print numbers from 0 to 5 in new line
+
+# -------------------------------------------------------------------------------------
+# for loop
