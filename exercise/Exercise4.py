f1a74bc8b14641ee405f497aad25aa17058cb775
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 02:39:17 +0530

    modify exercise
    instead of 1 to 20 i am taking the range from user and printing the odd number and as well as number odd number present in that range

--- exercise/Exercise4.py
+++ exercise/Exercise4.py
@@ -1,13 +1,19 @@
 
 '''
 
-Print the odd number from 1 to 20
+Take the range from user and print the odd number in that range
 
 '''
 
-for num in range(1,21):
+start_ = int(input("Enter start range: "))
+end_ = int(input("Enter end value: "))
+odd_nu = 0
+
+for num in range(start_, end_ + 1):
     if num % 2 == 0:
         continue
     print(num)
+    odd_nu += 1
 
+print("There are %d odd  number from %d to %d" %(odd_nu, start_, end_))
 
