e20ec4db4bb5c0115feaa52d1e5deb3d8b19a459
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 02:06:25 +0530

    added in git and learned about range()

---
+++ loops_in_python.py
@@ -0,0 +1,9 @@
+
+# Loops in python
+
+# range(start, stop, steps) => range() will provide a sequence of number starting from start /0(by default) to end-1
+num = range(5)
+print(num)
+
+# -------------------------------------------------------------------------------------
+# while loop
