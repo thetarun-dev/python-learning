b86ec168495da75094aea3f8d2dbb59bb8b50c1d
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 02:17:21 +0530

    learned about the for loop

--- loops_in_python.py
+++ loops_in_python.py
@@ -27,3 +27,21 @@ while counter <= 5: # condition is counter should be smaller or equal to 5
 
 # -------------------------------------------------------------------------------------
 # for loop
+
+'''
+
+Syntax
+
+for i in sequence:
+    do something
+
+We used the for loop when we want to iterate it through some sequence/data/list, dicitonary, set data type etc
+'''
+
+# Example
+for i in range(6):  # by giving an range from 1 to 5 it will iterate the value and store it in i and unless the sequence 
+                    # end loop will continue
+    print(i)        # which we are gonna print
+
+
+
