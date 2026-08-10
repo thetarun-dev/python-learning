7b6e902ac0ca4c2d785c8f5523aa9deef3ac2a6e
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-11 03:08:13 +0530

    modify
    learned about the len() operator

--- list_datatype_in_python.py
+++ list_datatype_in_python.py
@@ -8,8 +8,18 @@ Example: [1, "Tarun", 89.89, False, 3]
 
 Like the array the list index start with 0
 
+List item can be added, remove or changed and duplicate value can be stored. List also have different method by which we can perform certain
+operations on list
+
+Link: https://www.w3schools.com/python/python_lists.asp
+
 '''
 
+# Example
 list1 = [1, "Tarun", 89.89, False, 3]
 print(list1)
+
+# length() => It will provide the length of the list/no of items
+print("Length of list1 is:", len(list1))      # 5
+
 
