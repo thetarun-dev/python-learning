e057c6f289b8a6b93bd48e7fb6fe5689606028db
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-12 18:53:53 +0530

    added set_.py

---
+++ set_.py
@@ -0,0 +1,28 @@
+
+# Set
+
+'''
+
+Set is a collection of items store in a single variables but it's unchangeable, unordered and unindexed and they don't allow
+duplicate value
+
+They are enclosed with {}
+
+Link: https://www.w3schools.com/python/python_sets_methods.asp
+
+'''
+
+# -------------------------------------------------------------------------------------
+# set 
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+# -------------------------------------------------------------------------------------
+# adding new item in a set using the add()
+# even though we cannot change the value which is already store in set we can add new and remove value to the set
+thisset.add("watermelon")
+print(thisset)
+
+# -------------------------------------------------------------------------------------
+
+
